026b2ae0de7d088ea6f81f5153f14cd1dbf6183b
gabrielomena <gabrielopfaria@gmail.com>
2023-02-11 14:46:24 -0400

    frontend

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,44 +1,43 @@
-const {User}= require('../models')
-const bcrypt = require('bcrypt')
-const yup = require('yup')
+const { User } = require("../models")
+const bcrypt = require("bcrypt")
+const yup = require("yup")
 
 class UserController {
-    static async store(req, res) {
-        const schema = yup.object().shape({
-            name: yup.string().required(),
-            email: yup.string().email().required(),
-            password: yup.string().required().min(6)
-        })
+  static async store(req, res) {
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+      password: yup.string().required().min(6),
+    })
 
-        try {
-            await schema.validateSync(req.body, { abortEarly: false })
-        } catch (err) {
-            return res.status(422).json({ error: err.errors })
-        }
-
-        const { name, email, password } = req.body
-
-        const emailExist = await User.findOne({ where: { email: email } })
-
-        if (emailExist) {
-            return res.status(400).json("Email já está sendo usado")
-        }
-
-        // create password
-        const salt = await bcrypt.genSalt(10)
-        const password_hash = await bcrypt.hash(password,salt)
-
-
-        try {
-            const newUser = await User.create({
-                name, 
-                email,
-                password: password_hash
-            })
-            return res.status(201).json(newUser)
-        } catch (error) {
-                return res.json(error)
-        }
+    try {
+      await schema.validateSync(req.body, { abortEarly: false })
+    } catch (err) {
+      return res.status(422).json({ error: err.errors })
     }
+
+    const { name, email, password } = req.body
+
+    const emailExist = await User.findOne({ where: { email: email } })
+
+    if (emailExist) {
+      return res.status(400).json("Email já está sendo usado")
+    }
+
+    // create password
+    const salt = await bcrypt.genSalt(10)
+    const password_hash = await bcrypt.hash(password, salt)
+
+    try {
+      const newUser = await User.create({
+        name,
+        email,
+        password: password_hash,
+      })
+      return res.status(201).json(newUser)
+    } catch (error) {
+      return res.json(error)
+    }
+  }
 }
 module.exports = UserController
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,30 +1,29 @@
-const auths = require('../config/auth')
-const jwt = require('jsonwebtoken')
+const auths = require("../config/auth")
+const jwt = require("jsonwebtoken")
 
-const auth = (req,res,next)=>{
-    const authToken = req.headers.authorization
-    // console.log(authToken);
+const auth = (req, res, next) => {
+  const authToken = req.headers.authorization
+  // console.log(authToken);
 
-    if(!authToken){
-        return res.status(401).json({error:"Token not provieded"})
-    }
+  if (!authToken) {
+    return res.status(401).json({ error: "Token not provieded" })
+  }
 
-    const token = authToken.split(' ')[1]
-    
-   try {
-    jwt.verify(token, auths.secret, function(err,decoded){
-        if(err){
-            throw new Error()
-        }
-        req.userId = decoded.id
-        req.userName = decoded.name
-        // console.log(decoded);
-        return next()
+  const token = authToken.split(" ")[1]
+
+  try {
+    jwt.verify(token, auths.secret, function (err, decoded) {
+      if (err) {
+        throw new Error()
+      }
+      req.userId = decoded.id
+      req.userName = decoded.name
+      // console.log(decoded);
+      return next()
     })
-   } catch (error) {
-        return res.status(401).json("Token is invalid")
-   }
-   
+  } catch (error) {
+    return res.status(401).json("Token is invalid")
+  }
 }
 
 module.exports = auth
